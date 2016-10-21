a10d9e9a060b5af2b955fd5102a4b49bdd53265e
ultragtx <ultragtx@gmail.com>
2016-10-21 15:29:41 +0800

    Replace various ports (8332->9332, 8333->9333, 18332->19332, 18333->19333, 28332->29332)

--- qa/rpc-tests/zmq_test.py
+++ qa/rpc-tests/zmq_test.py
@@ -22,7 +22,7 @@ class ZMQTest (LitecoinTestFramework):
         super().__init__()
         self.num_nodes = 4
 
-    port = 28332
+    port = 29332
 
     def setup_nodes(self):
         self.zmqContext = zmq.Context()
